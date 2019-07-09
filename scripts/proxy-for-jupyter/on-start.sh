28f384411de7765f2bd624e88924774518e02b2a
Christian-kam <chriskam91@gmail.com>
2019-07-09 10:00:44 +0200

    Updated on-start.sh script
    I updated the proxy script to extract the proxy server URL to a variable SERVER, so it's easier to see where you need to replace the sever with your own proxy server and only need to do it once. Additionally, I have added an explanation in the script on why we are excluding S3.

--- scripts/proxy-for-jupyter/on-start.sh
+++ scripts/proxy-for-jupyter/on-start.sh
@@ -10,18 +10,22 @@ set -e
 
 su - ec2-user -c "mkdir /home/ec2-user/.ipython/ && mkdir /home/ec2-user/.ipython/profile_default/ && mkdir /home/ec2-user/.ipython/profile_default/startup/ && touch /home/ec2-user/.ipython/profile_default/startup/00-startup.py"
 
-# Please replace proxy.local:3128 with the URL of your proxy server eg, proxy.example.com:80 and proxy.example.com:443
+# Please replace proxy.local:3128 with the URL of your proxy server eg, proxy.example.com:80 or proxy.example.com:443
+# Please note that we are excluding S3 because we do not want this traffic to be routed over the public internet, but rather through the S3 endpoint in the VPC.
 
-echo "export http_proxy='http://proxy.local:3128'" | tee -a /home/ec2-user/.profile >/dev/null
-echo "export https_proxy='http://proxy.local:3128'" | tee -a /home/ec2-user/.profile >/dev/null
+#PARAMETER
+SERVER=http://proxy.local:3128
+
+echo "export http_proxy='$SERVER'" | tee -a /home/ec2-user/.profile >/dev/null
+echo "export https_proxy='$SERVER'" | tee -a /home/ec2-user/.profile >/dev/null
 echo "export no_proxy='s3.amazonaws.com,127.0.0.1,localhost'" | tee -a /home/ec2-user/.profile >/dev/null
 
 # Now we change the terminal shell to bash
 echo "c.NotebookApp.terminado_settings={'shell_command': ['/bin/bash']}" | tee -a /home/ec2-user/.jupyter/jupyter_notebook_config.py >/dev/null
 
 echo "import sys,os,os.path" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
-echo "os.environ['HTTP_PROXY']="\""http://proxy.local:3128"\""" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
-echo "os.environ['HTTPS_PROXY']="\""http://proxy.local:3128"\""" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
+echo "os.environ['HTTP_PROXY']="\""$SERVER"\""" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
+echo "os.environ['HTTPS_PROXY']="\""$SERVER"\""" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
 echo "os.environ['NO_PROXY']="\""s3.amazonaws.com,127.0.0.1,localhost"\""" | tee -a /home/ec2-user/.ipython/profile_default/startup/00-startup.py >/dev/null
 
 # Next, we reboot the system so the bash shell setting can take effect. This reboot is only required when applying proxy settings to the shell environment as well.

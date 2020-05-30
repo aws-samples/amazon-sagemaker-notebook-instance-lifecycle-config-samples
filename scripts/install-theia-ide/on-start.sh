019c5c664b9811ce01fe8c5d54d1f5bb81b9cff2
Jason Barto <jason.p.barto@gmail.com>
2020-05-30 23:01:53 +0100

    enabled linting by default of various linters

--- scripts/install-theia-ide/on-start.sh
+++ scripts/install-theia-ide/on-start.sh
@@ -30,26 +30,21 @@
 set -e
 
 sudo -u ec2-user -i <<'EOP'
-
 #####################################
 ## INSTALL THEIA IDE FROM SOURCE
 #####################################
 EC2_HOME=/home/ec2-user
 mkdir ${EC2_HOME}/theia && cd ${EC2_HOME}/theia
-
 ### begin by installing NVM, NodeJS v10, and Yarn
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
 source ${EC2_HOME}/.nvm/nvm.sh
 nvm install 10
 nvm use 10
 npm install -g yarn
-
 ### now compile Theia-IDE from source, retrieving the configuration package.json from GitHub
 export NODE_OPTIONS=--max_old_space_size=4096
 curl https://raw.githubusercontent.com/jpbarto/amazon-sagemaker-notebook-instance-lifecycle-config-samples/master/scripts/install-theia-ide/package.json -o ${EC2_HOME}/theia/package.json
 nohup yarn &
-
-
 #####################################
 ### Configure Theia defaults
 #####################################
@@ -57,15 +52,21 @@ THEIA_PATH=$PATH
 mkdir ${EC2_HOME}/.theia
 mkdir -p ${EC2_HOME}/SageMaker/.theia
 curl https://raw.githubusercontent.com/jpbarto/amazon-sagemaker-notebook-instance-lifecycle-config-samples/master/scripts/install-theia-ide/launch.json -o ${EC2_HOME}/SageMaker/.theia/launch.json
-echo '{"workbench.iconTheme": "theia-file-icons","terminal.integrated.inheritEnv": true}' > ${EC2_HOME}/.theia/settings.json
-
-
+cat >${EC2_HOME}/.theia/settings.json <<EOS
+{
+    "workbench.iconTheme": "theia-file-icons",
+    "terminal.integrated.inheritEnv": true,
+    "python.linting.pylintEnabled": true,
+    "python.linting.flake8Enabled": true,
+    "python.linting.pycodestyleEnabled": true,
+    "python.linting.enabled": true
+}
+EOS
 #####################################
 ### Integrate Theia IDE with Jupyter
 #####################################
 ## CONFIGURE JUPYTER PROXY TO MAP TO THE THEIA IDE
 JUPYTER_ENV=/home/ec2-user/anaconda3/envs/JupyterSystemEnv
-
 source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
 cat >>${JUPYTER_ENV}/etc/jupyter/jupyter_notebook_config.py <<EOC
 c.ServerProxy.servers = {
@@ -77,7 +78,6 @@ c.ServerProxy.servers = {
   }
 }
 EOC
-
 pip install jupyter-server-proxy pylint autopep8 yapf pyflakes pycodestyle 'python-language-server[all]'
 jupyter serverextension enable --py --sys-prefix jupyter_server_proxy
 jupyter labextension install @jupyterlab/server-proxy

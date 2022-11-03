778a242ff86e08d5da388df366e37ed4787bcf48
tayo <113546485+tayodok@users.noreply.github.com>
2022-11-03 12:01:35 -0500

    Added logic to determine system restart based on AL and JL versions. (#96)
    Co-authored-by: Tayo Adedokun <tayodot@amazon.com>

--- scripts/set-env-variable/on-start.sh
+++ scripts/set-env-variable/on-start.sh
@@ -9,11 +9,24 @@ set -e
 #   1. Ensure the Notebook Instance execution role has permission of SageMaker:ListTags
 #
 
+#sudo -u ec2-user -i <<'EOF'
+
 # PARAMETERS
-YOUR_ENV_VARIABLE_NAME=your_env_variable_name
+YOUR_ENV_VARIABLE_NAME=<ENV_VAR_NAME>
 
 NOTEBOOK_ARN=$(jq '.ResourceArn' /opt/ml/metadata/resource-metadata.json --raw-output)
 TAG=$(aws sagemaker list-tags --resource-arn $NOTEBOOK_ARN  | jq -r --arg YOUR_ENV_VARIABLE_NAME "$YOUR_ENV_VARIABLE_NAME" .'Tags[] | select(.Key == $YOUR_ENV_VARIABLE_NAME).Value' --raw-output)
 touch /etc/profile.d/jupyter-env.sh
 echo "export $YOUR_ENV_VARIABLE_NAME=$TAG" >> /etc/profile.d/jupyter-env.sh
-initctl restart jupyter-server --no-wait
+
+# restart command is dependent on current running Amazon Linux and JupyterLab
+CURR_VERSION_AL=$(cat /etc/system-release)
+CURR_VERSION_JS=$(jupyter --version)
+
+if [[ $CURR_VERSION_JS == *$"jupyter_core     : 4.9.1"* ]] && [[ $CURR_VERSION_AL == *$" release 2018"* ]]; then
+	sudo initctl restart jupyter-server --no-wait
+else
+	sudo systemctl --no-block restart jupyter-server.service || true
+fi
+
+#EOF

13b4023c9dca45fea58b2129fe5848619284653a
tayo <113546485+tayodok@users.noreply.github.com>
2022-11-15 19:39:41 -0600

    Using legacy print syntax compatible with python 2.7 and python 3. (#103)
    * Using legacy print syntax compatible with python 2.7 and python 3.
    
    * Changed syntax for python2.7 compatibility and on-start to check python3 first.
    
    Co-authored-by: Tayo Adedokun <tayodot@amazon.com>

--- scripts/auto-stop-idle/autostop.py
+++ scripts/auto-stop-idle/autostop.py
@@ -98,11 +98,11 @@ if len(data) > 0:
                         idle = False
                 else:
                     idle = False
-                    print(f'Notebook idle state set as {idle} because no kernel has been detected.')
+                    print('Notebook idle state set as %s because no kernel has been detected.' % idle)
             else:
                 if not is_idle(notebook['kernel']['last_activity']):
                     idle = False
-                    print(f'Notebook idle state set as {idle} since kernel connections are ignored.')
+                    print('Notebook idle state set as %s since kernel connections are ignored.' % idle)
         else:
             print('Notebook is not idle:', notebook['kernel']['execution_state'])
             idle = False
@@ -113,7 +113,7 @@ else:
     )['LastModifiedTime']
     if not is_idle(uptime.strftime("%Y-%m-%dT%H:%M:%S.%fz")):
         idle = False
-        print(f'Notebook idle state set as {idle} since no sessions detected.')
+        print('Notebook idle state set as %s since no sessions detected.' % idle)
 
 if idle:
     print('Closing idle notebook')
